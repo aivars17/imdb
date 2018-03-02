a82ab8d880c316a8303a77293a3e329be21c868b
aivars17 <amalsevskis@gmail.com>
2018-03-02 12:17:43 +0000

    seed run fix

--- database/seeds/MoviesSeeder.php
+++ database/seeds/MoviesSeeder.php
@@ -38,7 +38,7 @@ class MoviesSeeder extends Seeder
                     $category = Categories::create([
                         'name' => $category_name,
                         'description' => 'Nauji',
-                        'user_id' => 3,
+                        'user_id' => 1,
                     ]);
                     $cat_id = $category->id;
                 }
@@ -47,7 +47,7 @@ class MoviesSeeder extends Seeder
                 $movie = Movie::create([
                     'name' => $movies['results'][$y]['title'],
                     'category_id' => $cat_id,
-                    'user_id' => 3,
+                    'user_id' => 1,
                     'description' => $movies['results'][$y]['overview'],
                     'year' => $movies['results'][$y]['release_date'],
                     'rating' => $movies['results'][$y]['vote_average'],
@@ -59,7 +59,7 @@ class MoviesSeeder extends Seeder
                 Storage::put('public/image/' . $filename, $file);
                 $movie->image()->create([
                     'filename' => $filename,
-                    'user_id' => 3,
+                    'user_id' => 1,
                 ]);
 
                 $file = file_get_contents('https://image.tmdb.org/t/p/original' . $movies['results'][$y]['backdrop_path']);
@@ -67,7 +67,7 @@ class MoviesSeeder extends Seeder
                 Storage::put('public/image/' . $filename, $file);
                 $movie->image()->create([
                     'filename' => $filename,
-                    'user_id' => 3,
+                    'user_id' => 1,
                 ]);
 
                 $casts = 'http://api.themoviedb.org/3/movie/'.$movies['results'][$y]['id'].'/casts?api_key=be98f6d22107b703991b078fdf1aeb9c';
@@ -106,7 +106,7 @@ class MoviesSeeder extends Seeder
                         'name' => $cast_detale['name'],
                         'birthday' => $birthday,
                         'deathday' => $deathday,
-                        'user_id' => 3,
+                        'user_id' => 1,
 
                     ]);
                     $actor->movie()->attach($movie->id);
@@ -116,7 +116,7 @@ class MoviesSeeder extends Seeder
                         Storage::put('public/image/' . $filename, $file);
                         $actor->image()->create([
                             'filename' => $filename,
-                            'user_id' => 3,
+                            'user_id' => 1,
                         ]);
                     }
 
