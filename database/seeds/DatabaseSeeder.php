f2b0630e42e47e871e943d7b10dfd3d3d34952f1
aivars17 <amalsevskis@gmail.com>
2018-03-02 12:28:30 +0000

    seed run fix

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
 
-        $this->call([MoviesSeeder::class,UserSeeder::class]);
+        $this->call(MoviesSeeder::class);
     }
 }
