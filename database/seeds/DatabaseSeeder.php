2ce971002a00ec5eb5daae5961b32000bfab70aa
aivars17 <30680298+aivars17@users.noreply.github.com>
2018-03-18 03:27:33 +0200

    Update DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
 
-        $this->call([MoviesSeeder::class, UserSeeder::class]);
+        $this->call([UserSeeder::class, MoviesSeeder::class]);
     }
 }
